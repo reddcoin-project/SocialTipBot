a70b2a10b02383c77f8967c77a5b71f965a4eb4b
Bin Ren <bin.ren@gmail.com>
2014-02-21 00:54:44 +0000

    insert or replace into

--- src/ctb/ctb_user.py
+++ src/ctb/ctb_user.py
@@ -242,7 +242,7 @@ class CtbUser(object):
 
         # Add coin addresses to database
         # sql_addr = "UPDATE t_addrs SET username=:u, network=:n, coin=:c, address=:a"
-        sql_addr = "INSERT INTO t_addrs (username, network, coin, address) VALUES (:u, :n, :c, :a)"
+        sql_addr = "INSERT OR REPLACE INTO t_addrs (username, network, coin, address) VALUES (:u, :n, :c, :a)"
         for c, a in new_addrs.iteritems():
             try:
                 sqlexec = self.ctb.db.execute(sql_addr, {'u': self.name.lower(), 'n': self.network.lower(),
