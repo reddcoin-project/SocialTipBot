0114f50bacbd0f2eb43221d4b04fce1831f7a0b9
laudney <reddtipbot@gmail.com>
2014-02-28 22:35:48 +0000

    test bot for Twitch chat

---
+++ src/ctb/ctb_twitch.py
@@ -0,0 +1,89 @@
+__author__ = 'bren'
+
+
+"""A simple example bot.
+
+This is an example bot that uses the SingleServerIRCBot class from
+irc.bot.  The bot enters a channel and listens for commands in
+private messages and channel traffic.  Commands in channel messages
+are given by prefixing the text by the bot name followed by a colon.
+It also responds to DCC CHAT invitations and echos data sent in such
+sessions.
+
+The known commands are:
+
+    stats -- Prints some channel information.
+
+    disconnect -- Disconnect the bot.  The bot will try to reconnect
+                  after 60 seconds.
+
+    die -- Let the bot cease to exist.
+"""
+
+import irc.bot
+import irc.strings
+
+
+class TestBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, password, server, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, password)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        pass
+
+    def on_dccchat(self, c, e):
+        pass
+
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = chobj.users()
+                users.sort()
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = chobj.opers()
+                opers.sort()
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = chobj.voiced()
+                voiced.sort()
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+
+
+def main():
+    server = 'irc.twitch.tv'
+    port = 6667
+    channel = 'tipredd'
+    nickname = 'tipreddcoin'
+    password = 'oauth:pi5oz89kcioi5650yo1qeo5xwppawq3'
+
+    bot = TestBot(channel, nickname, password, server, port)
+    bot.start()
+
+if __name__ == "__main__":
+    main()
