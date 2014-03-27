9eeb5674aefb7ffa9f4d3d6d58cdec941464c195
laudney <reddtipbot@gmail.com>
2014-03-27 23:02:36 +0000

    create Economy class for stats

--- src/economy/stats.py
+++ src/economy/stats.py
@@ -4,27 +4,57 @@ __author__ = 'bren'
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, Numeric, UnicodeText
 from sqlalchemy.pool import SingletonThreadPool
 
+from datetime import datetime
+import pytz
 import pandas as pd
 
 
-def connect_db(dsn_url):
+class Economy:
     metadata = MetaData()
-    engine = create_engine(dsn_url, echo_pool=True, poolclass=SingletonThreadPool,
-                           execution_options={'autocommit': True})
-    metadata.create_all(engine)
-    return engine
+
+    def __init__(self, dsn_url):
+        """
+        Pass a DSN URL conforming to the SQLAlchemy API
+        """
+        self.dsn_url = dsn_url
+        self.db = self.connect()
+
+    def connect(self):
+        """
+        Return a connection object
+        """
+        engine = create_engine(self.dsn_url, echo_pool=True, poolclass=SingletonThreadPool,
+                               execution_options={'autocommit': True})
+        self.metadata.create_all(engine)
+        return engine.connect()
+
+    def top_tippers(self):
+        sql = "SELECT from_user as user, sum(coin_val) AS amount FROM t_action WHERE type = 'givetip' "
+        sql += "AND state != 'failed' GROUP BY from_user ORDER BY amount DESC"
+        sqlexec = self.db.execute(sql)
+        results = pd.DataFrame(sqlexec.fetchall(), columns=sqlexec.keys())
+        return results
+
+    def top_receivers(self):
+        sql = "SELECT to_user as user, sum(coin_val) AS amount FROM t_action WHERE type = 'givetip' "
+        sql += "AND state = 'completed' GROUP BY to_user ORDER BY amount DESC"
+        sqlexec = self.db.execute(sql)
+        results = pd.DataFrame(sqlexec.fetchall(), columns=sqlexec.keys())
+        return results
+
+    def recent_transactions(self):
+        sql = "SELECT from_user, to_user, coin_val AS amount, created_utc AS timestamp FROM t_action "
+        sql += "WHERE type = 'givetip' AND state = 'completed' ORDER BY timestamp DESC"
+        sqlexec = self.db.execute(sql)
+        results = pd.DataFrame(sqlexec.fetchall(), columns=sqlexec.keys())
+        dts = map(datetime.fromtimestamp, results['timestamp'])
+        results['timestamp'] = map(pytz.utc.localize, dts)
+        return results
 
 
 if __name__ == '__main__':
     dsn_url = 'sqlite:///../db/twitter-live1.db'
-    db = connect_db(dsn_url)
-    conn = db.connect()
-
-    # top tippers
-    sql1 = "SELECT from_user as user, sum(coin_val) AS amount FROM t_action WHERE type = 'givetip' AND state != 'failed' GROUP BY from_user ORDER BY amount DESC"
-
-    # top receivers
-    sql2 = "SELECT to_user as user, sum(coin_val) AS amount FROM t_action WHERE type = 'givetip' AND state = 'completed' GROUP BY to_user ORDER BY amount DESC"
-
-    sqlexec = conn.execute(sql2)
-    results = pd.DataFrame(sqlexec.fetchall(), columns=sqlexec.keys())
+    economy = Economy(dsn_url)
+    tippers = economy.top_tippers()
+    receivers = economy.top_receivers()
+    transactions = economy.recent_transactions()
