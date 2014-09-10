bf166172da3272d27e6159e688ffdf54411015d4
laudney <reddtipbot@gmail.com>
2014-09-10 16:01:25 +0100

    Add script to identify non-existent Reddit accounts.

---
+++ src/maintenance/scavenge.py
@@ -0,0 +1,20 @@
+__author__ = 'laudney'
+
+
+import time
+import urllib2
+import simplejson
+
+
+if __name__ == '__main__':
+    accounts = simplejson.load(open('/Users/bren/reddit_accounts'))
+
+    for name in accounts.keys():
+        try:
+            urllib2.urlopen('http://www.reddit.com/user/' + name)
+        except urllib2.HTTPError as e:
+            print e.code
+            if e.code == 404:
+                print name, accounts[name]
+
+        time.sleep(3)
