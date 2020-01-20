22d688e9b9a334107c1a05da5c2accf99acce356
John Nash <redd@redd.ink>
2020-01-20 18:53:27 +1100

    add feature to check for followers and expire tips

--- src/ctb/ctb_webhooks.py
+++ src/ctb/ctb_webhooks.py
@@ -45,6 +45,68 @@ class TwitterWebHooks(object):
         self.conn = conn
         self.events_adapter = TwitterWebhookAdapter(self.app_secret, "/webhooks/twitter")
 
+    def follow_followers(self):
+        lg.debug('TwitterWebHooks::follow_followers(): started')
+
+        # only get the latest followers but this relies on Twitter returns the latest followers first
+        followers = []
+        for fid in self.conn.cursor(self.conn.get_followers_ids, count=5000):
+            followers.append(fid)
+
+        friends = []
+        for fid in self.conn.cursor(self.conn.get_friends_ids, count=5000):
+            friends.append(fid)
+
+        pending = []
+        for fid in self.conn.cursor(self.conn.get_outgoing_friendship_ids):
+            pending.append(fid)
+
+        lg.debug('TwitterWebHooks::follow_followers(): looking for new followers')
+        to_follow = [f for f in followers[:100] if f not in friends and f not in pending]
+        lg.debug('TwitterWebHooks::follow_followers(): about to send follow request')
+
+        # only follow 10 at a time
+        actions = []
+        for user_id in to_follow[:10]:
+            try:
+                resp = self.conn.create_friendship(user_id=user_id)
+                time.sleep(1)
+            except TwythonError as e:
+                # either really failed (e.g. sent request before) or already friends
+                lg.warning("TwitterWebHooks::follow_followers: failed to follow user %s: %s", user_id, e.msg)
+                continue
+            else:
+                lg.debug('TwitterWebHooks::follow_followers(): just sent request to follow user %s', user_id)
+
+            msg = {'id': str(user_id),
+                   'created_utc': self._timestamp_utc_now(),
+                   'author': {'name': resp['screen_name']},
+                   'body': '+register',
+                   'type': 'mention'}
+            # make the msg id unique
+            msg['id'] += ('@' + str(msg['created_utc']))
+
+            action = ctb_action.eval_message(ctb_misc.DotDict(msg), self.ctb)
+            actions.append(action)
+
+        return actions
+
+    def _parse_event(self, event_data):
+
+        # check pending tips
+        if (datetime.utcnow() - self.last_expiry).seconds > 60 * 60:
+            self.ctb.expire_pending_tips()
+            self.last_expiry = datetime.utcnow()
+
+        # do not process event more than once per minute
+        if (datetime.utcnow() - self.last_event).seconds > 60:
+            # actions = self.follow_followers()
+            # self.ctb.last_event = datetime.utcnow()
+            # return actions
+            return None
+        else:
+            return None
+
     def _parse_mention(self, event_data):
 
         event = event_data['event']
@@ -199,8 +261,6 @@ class TwitterWebHooks(object):
                     except Exception as e:
                         lg.info("An error occurred responding to Mention: {}".format(e))
 
-
-
         @self.events_adapter.on("favorite_events")
         def handle_message(event_data):
             event = event_data['event']
@@ -209,13 +269,27 @@ class TwitterWebHooks(object):
             faved_status_screen_name = faved_status['user']['screen_name']
             faved_by_screen_name = event['user']['screen_name']
             lg.info("@{} faved @{}'s tweet: {}".format(faved_by_screen_name, faved_status_screen_name, faved_status_id))
-            #print(json.dumps(event_data, indent=4, sort_keys=True))
+            # print(json.dumps(event_data, indent=4, sort_keys=True))
 
         @self.events_adapter.on("any")
         def handle_message(event_data):
             # Loop through events array and log received events
             for s in filter(lambda x: '_event' in x, list(event_data)):
                 lg.info("[any] Received event: {}".format(s))
+
+            actions = self._parse_event(event_data)
+
+            if not actions:
+                return
+
+            if not isinstance(actions, list):
+                actions = [actions]
+
+            for action in actions:
+                if action:
+                    lg.info("TwitterWebHooks::on_success(): %s from %s", action.type, action.u_from.name)
+                    lg.debug("TwitterWebHooks::on_success(): comment body: <%s>", action.msg.body)
+                    action.do()
 
 
         # Handler for error events
