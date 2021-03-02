883e39f1c7e13ee3e78198890164e0862cae46fa
John Nash <redd@redd.ink>
2021-03-02 16:24:53 +1100

    add yaml conf file reading

--- src/maintenance/follow_registered.py
+++ src/maintenance/follow_registered.py
@@ -6,12 +6,17 @@ import traceback
 from twython import Twython, TwythonStreamer, TwythonRateLimitError, TwythonError
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, Numeric, UnicodeText
 from sqlalchemy.pool import SingletonThreadPool
+import yaml
 
 
 if __name__ == '__main__':
     # connect to database
     metadata = MetaData()
-    dsn_url = 'sqlite:///../db/twitter-live1.db'
+    #dsn_url = 'sqlite:///../db/twitter-live1.db'
+    with open(r'../conf/db.yml') as file:
+        conf = yaml.full_load(file)
+
+    dsn_url = "mysql+mysqldb://%s:%s@%s:%s/%s?charset=utf8" % (conf["auth"]["user"], conf["auth"]["password"], conf["auth"]["host"], conf["auth"]["port"], conf["auth"]["dbname"])
     engine = create_engine(dsn_url, echo_pool=True, poolclass=SingletonThreadPool,
                            execution_options={'autocommit': True})
     metadata.create_all(engine)
@@ -24,11 +29,13 @@ if __name__ == '__main__':
         registered.append(sqlrow['from_user'])
 
     # now follow these registered users
-    user = 'tipreddcoin'
-    app_key = '9HDzugNwT22Xlcw0EGQfg'
-    app_secret = '8OjJWIdIpErtBd8RcYAhwt9kixoIfOPA1GiM6mf0M'
-    oauth_token = '2357688096-NtaUt52rHmqCCX2hz9t1gEcTjMnEbsvdX0pRZqp'
-    oauth_token_secret = 'TjUHsf68MWCRzCtjjhB05S6OEdCjPMQIU00rL8dH0SH5a'
+    with open(r'../conf/twitter/twitter.yml') as file:
+        conf = yaml.full_load(file)
+    user = conf["auth"]["user"]
+    app_key = conf["auth"]["app_key"]
+    app_secret = conf["auth"]["app_secret"]
+    oauth_token = conf["auth"]["oauth_token"]
+    oauth_token_secret = conf["auth"]["oauth_token_secret"]
 
     twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
